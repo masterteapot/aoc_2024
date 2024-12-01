eb228d1faaa1de919213239f009609952f08577e
Jared Kane <jared.s.kane@gmail.com>
2024-12-01 14:43:17 +0400

    fixing printing formatting a little

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,11 @@
 open Aoc_2024
 
 let () =
-  print_newline ();
-  print_endline "--- AOC 2024 in Ocaml ---";
-  print_endline "-- Day 1 Results -- ";
+  print_endline "\n------- AOC 2024 in Ocaml -------\n";
+  print_endline "--- Day 1 ---";
   print_endline "Part A:";
   print_int (Day_1.part_a "data/inputs/day_1.txt");
-  print_newline ();
-  print_endline "Part B:";
+  print_endline "\n\nPart B:";
   print_int (Day_1.part_b "data/inputs/day_1.txt");
-  print_endline ""
+  print_endline "\n---";
 ;;
