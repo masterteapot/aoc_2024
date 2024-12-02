2cdea466d8307119ce5f04bda8f15b2375c2c46e
Jared Kane <jared.s.kane@gmail.com>
2024-12-02 09:57:11 +0400

    solved day 2

--- lib/day_2.ml
+++ lib/day_2.ml
@@ -1,3 +1,8 @@
+type direction =
+  | Increasing
+  | Decreasing
+  | Unknown
+
 let read_file fname = Batteries.File.lines_of fname |> Batteries.List.of_enum
 
 let split_lines ~char txt_lines =
@@ -15,23 +20,52 @@ let find_matches left right =
 ;;
 
 let transform ls =
-    let rec aux acc = function
-        | [] -> List.rev acc
-        | hd :: tl -> aux (hd :: acc) tl
-    in
-    aux [] ls
+  let rec aux acc dir = function
+    | [] -> acc
+    | [ _ ] -> acc
+    | hd :: md :: tl ->
+      let new_dir =
+        if md > hd then Increasing else if md < hd then Decreasing else Unknown
+      in
+      let diff = Int.abs @@ (hd - md) in
+      let new_acc = acc && diff <= 3 && diff > 0 && (new_dir = dir || dir = Unknown) in
+      aux new_acc (if dir = Unknown then new_dir else dir) (md :: tl)
+  in
+  aux true Unknown ls
 ;;
 
-let print ls =
-    List.iter print_endline ls
+let rec make_range bottom top acc =
+  if bottom = top then acc else make_range bottom (top - 1) ((top - 1) :: acc)
 ;;
 
-(* let part_a fname = *)
-let fname = "data/examples/day_2a.txt" in
-let txt_lines = read_file fname in
-let split = split_lines ~char:' ' txt_lines in
-let trans = transform split in
-List.iter print trans;
+let print ls = List.iter print_endline ls
+
+let part_a fname =
+  let txt_lines = read_file fname in
+  let split =
+    split_lines ~char:' ' txt_lines |> List.map (fun x -> List.map int_of_string x)
+  in
+  let trans = List.map transform split in
+  List.fold_left (fun acc x -> if x = true then acc + 1 else acc) 0 trans
+;;
+
+let part_b fname =
+  let txt_lines = read_file fname in
+  let split =
+    split_lines ~char:' ' txt_lines |> List.map (fun x -> List.map int_of_string x)
+  in
+  let trans =
+    List.map
+      (fun ls ->
+        let nums = make_range 0 (List.length ls) [] in
+        List.fold_left
+          (fun acc n -> acc || transform (Batteries.List.remove_at n ls))
+          false
+          nums)
+      split
+  in
+  List.fold_left (fun acc x -> if x = true then acc + 1 else acc) 0 trans
+;;
 
 (* let part_b fname =  *)
 (*     let txt_lines = read_file fname in *)
--- tests/test.ml
+++ tests/test.ml
@@ -13,18 +13,16 @@ let tests =
           assert_equal
             31
             (Day_1.part_b "/home/jared/Projects/aoc_2024/data/examples/day_1b.txt"))
-       (* ; ("day 2 part a example" *)
-       (*    >:: fun _ -> *)
-       (*    assert_equal *)
-       (*      0 *)
-       (*      (Day_2.part_a "/home/jared/Projects/aoc_2024/data/examples/day_2a.txt")) *)
-       (* ; ("day 2 part b example" *)
-       (*    >:: fun _ -> *)
-       (*    assert_equal *)
-       (*      0 *)
-       (*      (Day_2.part_b "/home/jared/Projects/aoc_2024/data/examples/day_2b.txt")) *)
-         (*    >:: fun _ -> assert_raises (Failure "nth") (fun () -> nth [ "a" ] 2)) *)
-         (* ; ("length of a list" >:: fun _ -> assert_equal 3 (length [ "a"; "b"; "c" ])) *)
+       ; ("day 2 part a example"
+          >:: fun _ ->
+          assert_equal
+            2
+            (Day_2.part_a "/home/jared/Projects/aoc_2024/data/examples/day_2a.txt"))
+       ; ("day 2 part b example"
+          >:: fun _ ->
+          assert_equal
+            4
+            (Day_2.part_b "/home/jared/Projects/aoc_2024/data/examples/day_2b.txt"))
        ]
 ;;
 
